5d5aba194ba211fa541b6aae42114d185ec4fbfc
Johnny Proano <jproano22@yahoo.com>
2023-02-15 23:11:26 -0500

    created renderleads function, and renderleads within addeventlistner

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
-let inputButton = document.getElementById("input-button");
+let myLeads = [];
+const inputEl = document.getElementById("input-el");
+const inputButton = document.getElementById("input-button");
+const ulEl = document.getElementById("ul-el");
 
 inputButton.addEventListener("click", function () {
-  console.log("clicked");
+  myLeads.push(inputEl.value);
+  console.log(myLeads);
+  renderLeads();
 });
+
+function renderLeads() {
+  let listItems = "";
+  for (let i = 0; i < myLeads.length; i++) {
+    listItems += "<li>" + myLeads[i] + "</li>";
+  }
+  ulEl.innerHTML = listItems;
+}
