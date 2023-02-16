594feaf8cbe6c19896be887d0cb7a78ef6d2448b
Johnny Proano <jproano22@yahoo.com>
2023-02-16 00:33:45 -0500

    ready for MVP deploy and image and manifest.json

--- index.js
+++ index.js
@@ -6,13 +6,19 @@ const ulEl = document.getElementById("ul-el");
 inputButton.addEventListener("click", function () {
   myLeads.push(inputEl.value);
   console.log(myLeads);
+  inputEl.value = "";
   renderLeads();
 });
 
 function renderLeads() {
   let listItems = "";
   for (let i = 0; i < myLeads.length; i++) {
-    listItems += "<li>" + myLeads[i] + "</li>";
+    listItems += `
+      <li>
+        <a target='_blank' href='${myLeads[i]}'>  
+        ${myLeads[i]}
+        </a>
+      </li>`;
   }
   ulEl.innerHTML = listItems;
 }
