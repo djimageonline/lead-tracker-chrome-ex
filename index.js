df256674f0e2b43f5a51e3a067dcb8927a2c4812
Johnny Proano <jproano22@yahoo.com>
2023-02-17 23:06:20 -0500

    refactored render function, added save btn, and save btn listner

--- index.js
+++ index.js
@@ -2,13 +2,41 @@ let myLeads = [];
 const inputEl = document.getElementById("input-el");
 const inputButton = document.getElementById("input-button");
 const ulEl = document.getElementById("ul-el");
-
-let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+const deleteButton = document.getElementById("delete-button");
+const saveTabButton = document.getElementById("saveTab-button");
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 
 if (leadsFromLocalStorage) {
   myLeads = leadsFromLocalStorage;
-  renderLeads();
+  render(myLeads);
 }
+
+saveTabButton.addEventListener("click", function () {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    myLeads.push(tabs[0].url);
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+    render(myLeads);
+  });
+});
+
+function render(leads) {
+  let listItems = "";
+  for (let i = 0; i < leads.length; i++) {
+    listItems += `
+      <li>
+        <a target='_blank' href='${leads[i]}'>  
+        ${leads[i]}
+        </a>
+      </li>`;
+  }
+  ulEl.innerHTML = listItems;
+}
+
+deleteButton.addEventListener("dblclick", function () {
+  localStorage.clear();
+  myLeads = [];
+  render(myLeads);
+});
 
 inputButton.addEventListener("click", function () {
   myLeads.push(inputEl.value);
@@ -16,18 +44,5 @@ inputButton.addEventListener("click", function () {
   inputEl.value = "";
 
   localStorage.setItem("myLeads", JSON.stringify(myLeads));
-  renderLeads();
+  render(myLeads);
 });
-
-function renderLeads() {
-  let listItems = "";
-  for (let i = 0; i < myLeads.length; i++) {
-    listItems += `
-      <li>
-        <a target='_blank' href='${myLeads[i]}'>  
-        ${myLeads[i]}
-        </a>
-      </li>`;
-  }
-  ulEl.innerHTML = listItems;
-}
